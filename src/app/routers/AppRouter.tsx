b696fc26234ac5594d63b5900a97edec9bafcecd
Santiago Arias <sanx349@gmail.com>
2020-12-10 17:03:33 -0500

    Implement basic routing

---
+++ src/app/routers/AppRouter.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import SignUp from '../screens/SignUp';
+import Login from '../screens/Login';
+
+import { ROUTES } from './constants';
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Route path={ROUTES.login} component={Login} exact />
+      <Route path={ROUTES.signup} component={SignUp} exact />
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;
